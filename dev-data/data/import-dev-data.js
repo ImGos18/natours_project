d12d2c37278d6c41d0e1bae7feda564dde08fa5a
ImGos18 <gustavoesoler0@gmail.com>
2026-05-07 18:12:01 -0400

    curso

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -22,9 +22,7 @@ mongoose
   });
 
 //readJson
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 //import data
 
--- models/toursModel.js
+++ models/toursModel.js
@@ -89,7 +89,17 @@ const tourSchema = new mongoose.Schema(
           type: String,
           default: 'Point',
           enum: ['Point']
-        }
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
       }
     ]
   },
