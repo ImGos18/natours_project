71fa614ca7f2581f73fffa89023fa8817d973a00
ImGos18 <gustavoesoler0@gmail.com>
2026-05-28 15:56:32 -0400

    continuacion

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,13 +2,15 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter;
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
   res.status(200).json({ status: 'success', data: { reviews } });
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  //llow nested routes
-  if (!req.body.tour) req.body.tour = req.params.tourID;
+  //allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 
   const { tour, review, rating } = req.body;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
+router.use('/:tourId/reviews', reviewRouter);
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -33,12 +34,12 @@ router
 // GET  /tour/16565s/reviews
 // GET  /tour/16565s/reviews/555621314
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('users'),
-    reviewController.createReview
-  );
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
 
 module.exports = router;
